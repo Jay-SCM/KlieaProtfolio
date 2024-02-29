4ced22943c7243365f89a67786c212c1f76e8bd8
Kliea <John.Farmer1086@gmail.com>
2024-02-28 21:06:44 -0800

    trying to framer motion ..

--- components/ContentCard.js
+++ components/ContentCard.js
@@ -1,6 +1,3 @@
-
-
-// Import React library and motion module from framer-motion library
 import React from 'react';
 import { motion } from 'framer-motion';
 // Import styles from the Home module CSS file
@@ -31,24 +28,24 @@ const ContentCard = ({ children }) => {
 
 {/* TypeScript Icon */}
 <motion.img
-    src="/ts.png"
-    alt="TypeScript Icon"
-    className="animatedIcon"
-    style={{ width: '60px', height: '60px' }}
-    initial={{ opacity: 0, x: '-100%', y: '-100%' }}
-    animate={{ opacity: 1, x: '-650%', y: '-170%' }}
-    transition={{ duration: 1, delay: 1 }}
+ src="/ts.png"
+ alt="TypeScript Icon"
+ className="animatedIcon"
+ style={{ width: '60px', height: '60px' }}
+ initial={{ opacity: 0, x: '-100%', y: '-100%' }}
+ animate={{ opacity: 1, x: '-650%', y: '-170%' }}
+ transition={{ duration: 1, delay: 0.2 }}
 />
 
 {/* MySQL Icon */}
 <motion.img
-    src="/mysql.png"
-    alt="MySQL Icon"
-    className="animatedIcon"
-    style={{ width: '60px', height: '60px' }}
-    initial={{ opacity: 0, x: '-100%', y: '-100%' }}
-    animate={{ opacity: 1, x: '-520%', y: '-260%' }}
-    transition={{ duration: 1, delay: 1 }}
+                src="/mysql.png"
+                alt="MySQL Icon"
+                className="animatedIcon"
+                style={{ width: '60px', height: '60px' }}
+                initial={{ opacity: 0, x: '-100%', y: '-100%' }}
+                animate={{ opacity: 1, x: '-520%', y: '-260%' }}
+                transition={{ duration: 1, delay: 0.4 }}
 />
 
 {/* Figma Icon */}
@@ -94,48 +91,31 @@ const ContentCard = ({ children }) => {
     animate={{ opacity: 1, x: '700%', y: '-660%' }}
     transition={{ duration: 1, delay: 1 }}
 />
-            {/* Render an animated image (MySQL Icon) using framer-motion */}
-            <motion.img
+
+{/* MongoDB Icon */}
+<motion.img
     src="/mongodb.png"
-    alt="mongodb Icon"
+    alt="MongoDB Icon"
     className="animatedIcon"
     style={{ width: '60px', height: '60px' }}
-    initial={{ opacity: 0, x: '-100%', y: '-100%' }} // Initial position and opacity
-    animate={{ opacity: 1, x: '600%', y: '-760%' }} // Fade in the image and move into position
-    transition={{ duration: 1, delay: 1 }} // Transition duration and delay
-    whileHover={{ scale: 1.2 }} // Scale up the image when hovered
+    initial={{ opacity: 0, x: '-100%', y: '-100%' }}
+    animate={{ opacity: 1, x: '600%', y: '-760%' }}
+    transition={{ duration: 1, delay: 1 }}
+    whileHover={{ scale: 1.2 }}
 />
 
-            {/* Render an animated image (Figma Icon) using framer-motion */}
-            <motion.img
+{/* Next.js Icon */}
+<motion.img
     src="/next.png"
-    alt="nextjs Icon"
+    alt="Next.js Icon"
     className="animatedIcon"
     style={{ width: '60px', height: '60px' }}
-    initial={{ opacity: 0, x: '-100%', y: '-100%' }} // Initial position and opacity
-    animate={{ opacity: 1, x: '480%', y: '-860%' }} // Fade in the image and move into position
-    transition={{ duration: 1, delay: 1 }} // Transition duration and delay
-    whileHover={{ scale: 1.2 }} // Scale up the image when hovered
+    initial={{ opacity: 0, x: '-100%', y: '-100%' }}
+    animate={{ opacity: 1, x: '480%', y: '-860%' }}
+    transition={{ duration: 1, delay: 1 }}
+    whileHover={{ scale: 1.2 }}
 />
 
-            {/* Render an animated image (MUI Icon) using framer-motion */}
-            {/* <motion.img
-                src="/node-js.png"
-                alt="node-js Icon"
-                className="animatedIcon"
-                style={{ width: '60px', height: '60px' }}
-                animate={{ x: '350%', y: '-960%', repeat: Infinity, duration: 12000, delay: 2000 }}
-            /> */}
-
-            <motion.img
-                src="/node-js.png"
-                alt="node-js Icon"
-                className="animatedIcon"
-                style={{ width: '60px', height: '60px' }}
-                initial={{ opacity: 0, x: '0%', y: '0%' }} // Initial position and opacity
-                animate={{ opacity: 1, x: '350%', y: '-960%' }} // Final position and opacity
-                transition={{ duration: 1 }} // Transition duration in seconds
-            />
 
 
 
