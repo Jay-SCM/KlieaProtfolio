4ffcca6e0ecbb9e66be2703da087c44f0ada8a4f
Kliea <John.Farmer1086@gmail.com>
2024-03-01 04:36:29 -0800

    added chatap to project cards fixed githeadache

---
+++ components/ChatComponent.js
@@ -0,0 +1,43 @@
+// ChatComponent.js
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io(); // Connect to server
+
+const ChatComponent = () => {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      setMessages([...messages, data]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleMessageSend = () => {
+    socket.emit("message", { message: newMessage });
+    setNewMessage("");
+  };
+
+  return (
+    <div>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={newMessage}
+        onChange={(e) => setNewMessage(e.target.value)}
+      />
+      <button onClick={handleMessageSend}>Send</button>
+    </div>
+  );
+};
+
+export default ChatComponent;
---
+++ pages/ChatPage.js
@@ -0,0 +1,13 @@
+// pages/ChatPage.js
+import ChatComponent from '../components/ChatComponent';
+
+const ChatPage = () => {
+    return (
+        <div>
+            <h1>Chat Page</h1>
+            <ChatComponent />
+        </div>
+    );
+};
+
+export default ChatPage;
